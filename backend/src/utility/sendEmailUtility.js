08f82829f086013978bc0505fa491955c0044dd2
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-02-20 09:53:10 +0600

    mail

--- backend/src/utility/sendEmailUtility.js
+++ backend/src/utility/sendEmailUtility.js
@@ -1,33 +1,34 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
 const sendEmailUtility= async (EmailTo, EmailText, EmailSubject) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    const transporter = nodemailer.createTransport({
+/*    const transporter = nodemailer.createTransport({
         // host: 'mail.teamrabbil.com',
         host: 'smtp.ethereal.email',
         // port: 25,
         port: 587,
         secure: false,
         auth: {
-            /*user: "info@teamrabbil.com",
-            pass: '~sR4[bhaC[Qs'*/
+            /!*user: "info@teamrabbil.com",
+            pass: '~sR4[bhaC[Qs'*!/
             user: 'lonny.bahringer@ethereal.email',
             pass: 'mCGKAF1Yv8ghyQM72n'
         },tls: {
             rejectUnauthorized: false
         },
-    });
-
+    });*/
 
     const mailOptions = {
-        from: 'Task Manager MERN <info@teamrabbil.com>',
         to: EmailTo,
+        from: process.env.SENDGRID_EMAIL_FROM,
         subject: EmailSubject,
         text: EmailText
-    };
+    }
+
+    return sgMail.send(mailOptions);
 
 
-    return  await transporter.sendMail(mailOptions)
 
 }
 module.exports= sendEmailUtility
