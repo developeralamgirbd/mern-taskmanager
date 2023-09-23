4339e709cd0643fd0ac8877c3d3f13ed2938c769
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-09-23 22:15:27 +0600

    logger added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const app=require("./app");
+const {infoLogger} = require("./src/shared/logger");
 const PORT=process.env.PORT || 8080;
 
 app.listen(PORT,function () {
-    console.log(`Server run success on port ${PORT}`)
+    infoLogger.info(`app listening on port ${PORT}`);
 })
